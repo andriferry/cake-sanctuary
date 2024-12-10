d6f8a53e2349ea8280565cc177e93a0752a71e0d
andriferry <ferryandri35@gmail.com>
2024-12-10 21:47:13 +0700

    Add Login in

--- components/ui/form/FormItem.vue
+++ components/ui/form/FormItem.vue
@@ -2,14 +2,14 @@
 import { cn } from '@/lib/utils'
 import { useId } from 'radix-vue'
 import { type HTMLAttributes, provide } from 'vue'
-import { FORM_ITEM_INJECTION_KEY } from './injectionKeys'
+//import { FORM_ITEM_INJECTION_KEY } from './injectionKeys'
 
 const props = defineProps<{
   class?: HTMLAttributes['class']
 }>()
 
 const id = useId()
-provide(FORM_ITEM_INJECTION_KEY, id)
+// provide(FORM_ITEM_INJECTION_KEY, id)
 </script>
 
 <template>
