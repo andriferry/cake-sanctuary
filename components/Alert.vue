7b0d5a3f02dbe01d787c08b8d8b9d65201366d60
andriferry <ferryandri35@gmail.com>
2025-01-10 09:16:44 +0700

    Update Cart and shadow dropdown

--- components/Alert.vue
+++ components/Alert.vue
@@ -9,96 +9,116 @@
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end" class="text-secondary w-96 p-0">
             <Card variant="flat" class="text-secondary">
-                <CardHeader class="px-3.5">
+                <CardHeader class="px-3.5 pb-3 border-b border-secondary/10">
                     <CardTitle class="flex capitalize justify-between">
                         <span>Notifications</span>
 
                         <Badge> 5 New </Badge>
                     </CardTitle>
                 </CardHeader>
-                <CardContent class="px-3.5">
-                    <Button variant="ghost" size="full" class="justify-start">
-                        <div
-                            class="flex p-1 justify-between w-full items-center">
-                            <div class="flex gap-3">
+                <CardContent class="p-1">
+                    <ul class="[&_li:last-child]:border-0">
+                        <li class="border-b p-1.5 border-secondary/10">
+                            <Button
+                                variant="ghost"
+                                size="full"
+                                class="justify-start">
                                 <div
-                                    class="bg-primary/30 flex items-center justify-center rounded-full size-10">
-                                    <Icon
-                                        name="tabler:brand-paypal"
-                                        class="text-primary size-6"></Icon>
+                                    class="flex p-1 justify-between w-full items-center">
+                                    <div class="flex gap-3">
+                                        <div
+                                            class="bg-primary/30 text-primary flex items-center justify-center rounded-full size-10">
+                                            <Icon
+                                                name="tabler:brand-paypal"
+                                                class="size-6"></Icon>
+                                        </div>
+                                        <div
+                                            class="flex justify-start items-start flex-col">
+                                            <h6 class="font-medium text-base">
+                                                Paypal
+                                            </h6>
+                                            <span
+                                                class="truncate text-xs font-light text-secondary/70">
+                                                Received Payment for
+                                                <span class="font-medium">
+                                                    #3566
+                                                </span>
+                                            </span>
+                                        </div>
+                                    </div>
+
+                                    <small class="text-xs">Yesterday</small>
                                 </div>
+                            </Button>
+                        </li>
+
+                        <li class="border-b p-1.5 border-secondary/10">
+                            <Button
+                                variant="ghost"
+                                size="full"
+                                class="justify-start">
                                 <div
-                                    class="flex justify-start items-start flex-col">
-                                    <h6 class="font-medium text-base">
-                                        Paypal
-                                    </h6>
-                                    <span
-                                        class="truncate text-xs font-light text-secondary/70">
-                                        Received Payment for
-                                        <span class="font-medium">#3566</span>
-                                    </span>
+                                    class="flex p-1 justify-between w-full items-center">
+                                    <div class="flex gap-3">
+                                        <div
+                                            class="bg-secondary/30 text-secondary flex items-center justify-center rounded-full size-10">
+                                            <Icon
+                                                name="tabler:message"
+                                                class="size-6"></Icon>
+                                        </div>
+                                        <div
+                                            class="flex justify-start items-start flex-col">
+                                            <h6 class="font-normal text-base">
+                                                New message received
+                                            </h6>
+                                            <span
+                                                class="truncate text-xs font-light text-secondary/70">
+                                                You have new message from
+                                                customer
+                                            </span>
+                                            <small class="font-base truncate">
+                                                5 Hours Ago
+                                            </small>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+                            </Button>
+                        </li>
 
-                            <small class="text-xs">Yesterday</small>
-                        </div>
-                    </Button>
-
-                    <Button variant="ghost" size="full" class="justify-start">
-                        <div
-                            class="flex p-1 justify-between w-full items-center">
-                            <div class="flex gap-3">
+                        <li class="border-b p-1.5 border-secondary/10">
+                            <Button
+                                variant="ghost"
+                                size="full"
+                                class="justify-start">
                                 <div
-                                    class="bg-secondary/30 flex items-center justify-center rounded-full size-10">
-                                    <Icon
-                                        name="tabler:message"
-                                        class="text-secondary size-6"></Icon>
+                                    class="flex p-1 justify-between w-full items-center">
+                                    <div class="flex gap-3">
+                                        <div
+                                            class="bg-success/30 text-success flex items-center justify-center rounded-full size-10">
+                                            <Icon
+                                                name="tabler:circle-check"
+                                                class="size-6"></Icon>
+                                        </div>
+                                        <div
+                                            class="flex justify-start items-start flex-col">
+                                            <h6 class="font-normal text-base">
+                                                Congratulation New Order !
+                                            </h6>
+                                            <span
+                                                class="truncate text-xs font-light text-secondary/70">
+                                                You have new order from John Doe
+                                            </span>
+                                        </div>
+                                    </div>
+
+                                    <small class="text-xs">Today</small>
                                 </div>
-                                <div
-                                    class="flex justify-start items-start flex-col">
-                                    <h6 class="font-normal text-base">
-                                        New message received
-                                    </h6>
-                                    <span
-                                        class="truncate text-xs font-light text-secondary/70">
-                                        You have new message from customer
-                                    </span>
-                                </div>
-                            </div>
-
-                            <small class="text-xs">5 Hours Ago</small>
-                        </div>
-                    </Button>
-
-                    <Button variant="ghost" size="full" class="justify-start">
-                        <div
-                            class="flex p-1 justify-between w-full items-center">
-                            <div class="flex gap-3">
-                                <div
-                                    class="bg-success/30 flex items-center justify-center rounded-full size-10">
-                                    <Icon
-                                        name="tabler:circle-check"
-                                        class="text-success size-6"></Icon>
-                                </div>
-                                <div
-                                    class="flex justify-start items-start flex-col">
-                                    <h6 class="font-normal text-base">
-                                        Congratulation New Order !
-                                    </h6>
-                                    <span
-                                        class="truncate text-xs font-light text-secondary/70">
-                                        You have new order from John Doe
-                                    </span>
-                                </div>
-                            </div>
-
-                            <small class="text-xs">Today</small>
-                        </div>
-                    </Button>
-
-
+                            </Button>
+                        </li>
+                    </ul>
                 </CardContent>
-                <CardFooter class="flex justify-between px-3.5 pb-3.5">
+                <CardFooter
+                    class="flex justify-between border-t border-secondary/10 px-3.5 py-3.5">
                     <Button size="full">Read All Notifications</Button>
                 </CardFooter>
             </Card>
