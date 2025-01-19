0ac1ae6636aed3b2bea6d9995033967ac5b23e47
andriferry <ferryandri35@gmail.com>
2025-01-19 15:30:05 +0700

    Update tabs order

--- components/Cart/Packaging.vue
+++ components/Cart/Packaging.vue
@@ -4,12 +4,12 @@
     <div class="flex flex-col gap-3">
         <Label> Packaging </Label>
 
-        <div class="p-3 border flex text-secondary rounded-lg">
-            <div class="flex items-center">
+        <div class="p-3 border grid grid-cols-12 text-secondary rounded-lg">
+            <div class="col-span-3 flex items-center">
                 <Icon name="tabler:paper-bag" class="size-9" />
             </div>
 
-            <div class="flex flex-col gap-2 pl-2">
+            <div class="col-span-7 flex flex-col gap-2">
                 <p class="text-sm font-medium">Paper Bag</p>
 
                 <NumberField :default-value="1" :min="0" class="w-20">
@@ -23,20 +23,17 @@
                     </NumberFieldContent>
                 </NumberField>
             </div>
-
-            <div class="flex justify-end items-end ml-auto">
-                <div>
-                    <p class="text-xs font-light">$ 1.00</p>
-                </div>
+            <div class="col-span-2 flex flex-col justify-between items-end">
+                <p class="text-xs font-light">$ 1.00</p>
             </div>
         </div>
 
-        <div class="p-3 border flex text-secondary rounded-lg">
-            <div class="flex items-center">
+        <div class="p-3 border grid grid-cols-12 text-secondary rounded-lg">
+            <div class="col-span-3 flex items-center">
                 <Icon name="tabler:shopping-bag" class="size-9" />
             </div>
 
-            <div class="flex flex-col gap-2 pl-2">
+            <div class="col-span-7 flex flex-col gap-2">
                 <p class="text-sm font-medium">Shopping Bag</p>
 
                 <NumberField :default-value="1" :min="0" class="w-20">
@@ -50,11 +47,8 @@
                     </NumberFieldContent>
                 </NumberField>
             </div>
-
-            <div class="flex justify-end items-end ml-auto">
-                <div>
-                    <p class="text-xs font-light">$ 3.00</p>
-                </div>
+            <div class="col-span-2 flex flex-col justify-between items-end">
+                <p class="text-xs font-light">$ 3.00</p>
             </div>
         </div>
     </div>
