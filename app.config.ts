4769aaa6d29abb9549076e6110bd97f8f51aa18f
andriferry <ferryandri35@gmail.com>
2025-01-19 11:18:59 +0700

    disabled a few pages

--- app.config.ts
+++ app.config.ts
@@ -16,28 +16,28 @@ export default defineAppConfig({
             to: '/pos',
             icon: 'tabler:layout-dashboard',
         },
-        {
-            title: 'Product',
-            to: '/product',
-            icon: 'tabler:package',
-        },
-        {
-            title: 'Orders',
-            to: '/order',
-            icon: 'tabler:notes',
-            badge: '2',
-            badgeClass: 'bg-primary',
-        },
-        {
-            title: 'Transaction',
-            to: '/transaction',
-            icon: 'tabler:arrows-right-left',
-        },
-        {
-            title: 'Settings',
-            to: '/settings',
-            icon: 'tabler:settings',
-        },
+        // {
+        //     title: 'Product',
+        //     to: '/product',
+        //     icon: 'tabler:package',
+        // },
+        // {
+        //     title: 'Orders',
+        //     to: '/order',
+        //     icon: 'tabler:notes',
+        //     badge: '2',
+        //     badgeClass: 'bg-primary',
+        // },
+        // {
+        //     title: 'Transaction',
+        //     to: '/transaction',
+        //     icon: 'tabler:arrows-right-left',
+        // },
+        // {
+        //     title: 'Settings',
+        //     to: '/settings',
+        //     icon: 'tabler:settings',
+        // },
     ],
     sidebar: {
         ...sidebar,
