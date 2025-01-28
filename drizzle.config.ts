2e553abab76879d61546dd15b3f0de9926bf30bd
andriferry <ferryandri35@gmail.com>
2025-01-28 09:37:30 +0700

    Start Connection db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
     dialect: 'sqlite',
-    schema: './server/database/schema.ts',
-    out: './server/database/migrations',
+    schema: './database/schema.ts',
+    out: './database/migrations',
     dbCredentials: {
       url: process.env.NUXT_DB_FILE_NAME!,
     },
--- server/utils/db-connect.ts
+++ server/utils/db-connect.ts
@@ -1,3 +1,3 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 
-export const dbConnect = drizzle(process.env.NUXT_DB_FILE_NAME);
+export const dbConnect = drizzle(process.env.NUXT_DB_FILE_NAME || '');
