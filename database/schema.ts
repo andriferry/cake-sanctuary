3b09bdcfadd188fb23167f4eeb0ae1b71c01b910
andriferry <ferryandri35@gmail.com>
2025-02-05 15:58:37 +0700

    remove a few another migration

--- database/schema.ts
+++ database/schema.ts
@@ -9,30 +9,3 @@ export const users = sqliteTable('users', {
     picture: text('picture').default(''),
     created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
 });
-
-export const paymentStatus = sqliteTable('paymentstatus', {
-    id: integer('id').primaryKey({ autoIncrement: true }),
-    payment: text('paymentstatus').notNull(),
-    created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
-});
-
-export const paymentMethod = sqliteTable('paymentmethod', {
-    id: integer('id').primaryKey({ autoIncrement: true }),
-    paymentMethod: text('paymentmethod').notNull(),
-    created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
-});
-
-export const invoices = sqliteTable('invoices', {
-    id: integer('id').primaryKey({ autoIncrement: true }),
-    invoice: text('invoice').notNull(),
-    paymentMethod: integer('paymentmethod').references(() => paymentMethod.id).notNull(),
-    paymentStatus: integer('paymentstatus').references(() => paymentStatus.id).notNull(),
-    totalAmount: integer('amount').notNull(),
-    collected: integer('collected').references(() => users.id),
-    created_at: text().default(sql`(CURRENT_TIMESTAMP)`),
-});
-
-export const postsRelations = relations(invoices, ({ one }) => ({
-    paymentMethod: one(paymentMethod),
-    paymentStatus: one(paymentStatus),
-}));
--- pages/login.vue
+++ pages/login.vue
@@ -46,10 +46,6 @@ const errorHandling = (error: any) => {
 };
 
 const onSubmit = async () => {
-    $toast('Default', {
-        description: `Test`,
-    });
-
     try {
         const dataValid = await form.value?.validate();
 
