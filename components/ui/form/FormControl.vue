b0ab8046d8ad46e34d677ca3b2e06211251a9e58
andriferry <ferryandri35@gmail.com>
2025-02-05 16:58:48 +0700

    Fix hint

--- components/ui/form/FormControl.vue
+++ components/ui/form/FormControl.vue
@@ -63,7 +63,7 @@ const inputClass = computed(() => {
             {{ label }}
 
 
-            <span v-if="hint" class="text-[0.75rem] text-secondary/60">
+            <span v-if="hint" class="text-[0.65rem] text-secondary/60">
                 {{ hint }}
             </span>
         </FormLabel>
@@ -102,20 +102,3 @@ const inputClass = computed(() => {
         <FormMessage :message="errorMessage" />
     </div>
 </template>
-
-
-<!-- <script setup lang="ts">
-import Input from '@/components/ui/Input.vue'
-import Label from '@/components/ui/Label.vue'
-</script>
-
-<template>
-  <div>
-    <div class="mb-2 flex justify-between gap-1">
-      <Label for="input-04" class="mb-0">Input with hint</Label>
-      <span class="text-sm text-muted-foreground">Optional</span>
-    </div>
-    <Input id="input-04" placeholder="Email" type="email" />
-  </div>
-</template> -->
-
