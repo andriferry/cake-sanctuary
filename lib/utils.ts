2daf20a768fb47e8fa7b4441b2b43d1a1e54754a
andriferry <ferryandri35@gmail.com>
2025-02-20 08:49:54 +0700

    Update format valueUpdater

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,14 +10,8 @@ export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
 
-export function valueUpdater<T extends Updater<any>>(
-  updaterOrValue: T,
-  ref: Ref,
-): any {
-  ref.value
-        = typeof updaterOrValue === 'function'
-      ? updaterOrValue(ref.value)
-      : updaterOrValue
+export function valueUpdater<T extends Updater<any>>(updaterOrValue: T, ref: Ref): any {
+  ref.value = typeof updaterOrValue === 'function' ? updaterOrValue(ref.value) : updaterOrValue
 }
 
 const MONTHS = [
