e14e2bf24639cba3f9e7d2e9252f3afcb6d8a912
andriferry <ferryandri35@gmail.com>
2025-02-22 16:23:47 +0700

    Develop db command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -116,12 +116,12 @@ export default defineNuxtConfig({
       externalizeIconsJson: true,
     },
   },
-  nitro: {
-    experimental: {
-      database: true,
-      websocket: true,
-    },
-  },
+  // nitro: {
+  //   experimental: {
+  //     database: true,
+  //     websocket: true,
+  //   },
+  // },
   // colorMode: {
   //     preference: 'system', // default value of $colorMode.preference
   //     fallback: 'light', // fallback value if not system preference found
